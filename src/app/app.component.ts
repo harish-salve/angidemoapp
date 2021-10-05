c2bfc6436b96881f57425c3fc077ad33b1b5bc0b
Aafrin Shaikh <aafrin.shaikh@glodom.local>
2021-10-05 12:32:39 +0530

    commit 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angidemoapp';
+  baseurl='https://springboot-demoapp.herokuapp.com/';
+
+  constructor(private obj: HttpClient)
+  { 
+console.log('in app compo')
+    obj.get(this.baseurl).subscribe(data=>console.log(data));
+    
+  }
 }
